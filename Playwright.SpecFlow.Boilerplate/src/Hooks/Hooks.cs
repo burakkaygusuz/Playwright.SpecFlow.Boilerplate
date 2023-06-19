992d26d39d42b80f0835bd2516fcf77e042151cf
Burak Kaygusuz <kaygusuzburak@gmail.com>
2023-06-19 11:08:35 +0300

    Refactor the initializing the test suite

--- Playwright.SpecFlow.Boilerplate/src/Hooks/Hooks.cs
+++ Playwright.SpecFlow.Boilerplate/src/Hooks/Hooks.cs
@@ -1,57 +1,53 @@
 ﻿using Microsoft.Playwright;
 
-namespace Playwright.SpecFlow.Boilerplate.Hooks;
-
-[Binding]
-public class Hooks
+namespace Playwright.SpecFlow.Boilerplate.Hooks
 {
-    private static IBrowser _browser = null!;
-    private static IPage _page = null!;
-    private readonly ScenarioContext _scenarioContext;
-
-    public Hooks(ScenarioContext scenarioContext)
+    [Binding]
+    public class Hooks
     {
-        _scenarioContext = scenarioContext;
-    }
+        private static IBrowser _browser;
+        private static IPage _page;
+        private readonly ScenarioContext _scenarioContext;
 
-    [BeforeTestRun]
-    public static async Task BeforeTestRunAsync()
-    {
-        var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
-        _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        public Hooks(ScenarioContext scenarioContext) => _scenarioContext = scenarioContext;
+
+        [BeforeTestRun]
+        public static async Task SetUpBrowserAsync()
         {
-            Headless = true,
-            SlowMo = 50,
-            Timeout = 60000
-        });
+            var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            var isCi = Environment.GetEnvironmentVariable("CI") == "true";
+            _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = isCi,
+                SlowMo = 50,
+                Timeout = 60000
+            });
+            _page = await (await _browser.NewContextAsync(new BrowserNewContextOptions
+            {
+                ViewportSize = new ViewportSize { Width = 1920, Height = 1080 },
+                BaseURL = "https://bing.com"
+            })).NewPageAsync();
+        }
 
-        var context = await _browser.NewContextAsync(new BrowserNewContextOptions
+        [AfterTestRun]
+        public static async Task TearDownBrowserAsync()
         {
-            ViewportSize = new ViewportSize { Width = 1920, Height = 1080 },
-            BaseURL = "https://bing.com"
-        });
+            await _browser.CloseAsync();
+            await _browser.DisposeAsync();
+        }
 
-        _page = await context.NewPageAsync();
-    }
+        [BeforeScenario]
+        public async Task SetUpScenarioAsync()
+        {
+            _scenarioContext["browser"] = _browser;
+            _scenarioContext["page"] = _page;
+            await _page.GotoAsync("/");
+        }
 
-    [AfterTestRun]
-    public static async Task AfterTestRunAsync()
-    {
-        await _browser.CloseAsync();
-        await _browser.DisposeAsync();
-    }
-
-    [BeforeScenario]
-    public async Task BeforeScenarioAsync()
-    {
-        _scenarioContext["browser"] = _browser;
-        _scenarioContext["page"] = _page;
-        await _page.GotoAsync("/");
-    }
-
-    [AfterScenario]
-    public static async Task AfterScenario()
-    {
-        await _page.CloseAsync();
+        [AfterScenario]
+        public static async Task TearDownScenarioAsync()
+        {
+            await _page.CloseAsync();
+        }
     }
 }
